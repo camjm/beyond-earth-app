e22946f1d4be0f8d166aea67fbe934af8f1cdba8
Cam <cam@mackay.net.nz>
2015-07-24 21:31:45 +1200

    update service models

--- src/BeyondEarthApp.Web.Api.Models/Building.cs
+++ src/BeyondEarthApp.Web.Api.Models/Building.cs
@@ -1,4 +1,6 @@
-﻿namespace BeyondEarthApp.Web.Api.Models
+﻿using System.Collections.Generic;
+
+namespace BeyondEarthApp.Web.Api.Models
 {
     public class Building
     {
@@ -9,5 +11,22 @@
         public int Cost { get; set; }
 
         public Technology Technology { get; set; }
+
+        #region Links
+
+        private List<Link> _links;
+
+        public List<Link> Links
+        {
+            get { return _links ?? (_links = new List<Link>()); }
+            set { _links = value; }
+        }
+
+        public void AddLink(Link link)
+        {
+            Links.Add(link);
+        }
+
+        #endregion
     }
 }
--- src/BeyondEarthApp.Web.Api.Models/Unit.cs
+++ src/BeyondEarthApp.Web.Api.Models/Unit.cs
@@ -1,4 +1,6 @@
-﻿namespace BeyondEarthApp.Web.Api.Models
+﻿using System.Collections.Generic;
+
+namespace BeyondEarthApp.Web.Api.Models
 {
     public class Unit
     {
@@ -11,5 +13,22 @@
         public short Strength { get; set; }
 
         public Technology Technology { get; set; }
+
+        #region Links
+
+        private List<Link> _links;
+
+        public List<Link> Links
+        {
+            get { return _links ?? (_links = new List<Link>()); }
+            set { _links = value; }
+        }
+
+        public void AddLink(Link link)
+        {
+            Links.Add(link);
+        }
+
+        #endregion
     }
 }
