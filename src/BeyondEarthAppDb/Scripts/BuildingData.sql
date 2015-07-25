7f4b9dace48903f9aa0ad375a1b83292c783bd96
Cam <cam@mackay.net.nz>
2015-07-25 22:16:50 +1200

    fixed data initialization scripts

--- src/BeyondEarthAppDb/Scripts/BuildingData.sql
+++ src/BeyondEarthAppDb/Scripts/BuildingData.sql
@@ -1,38 +1,38 @@
-﻿DECLARE @technologyId INT
+﻿DECLARE @buildingTechnologyId INT
 
-SELECT @technologyId = TechnologyId FROM Technology WHERE Name = 'Habitation';
-IF @technologyId IS NOT NULL
+SELECT @buildingTechnologyId = TechnologyId FROM Technology WHERE Name = 'Habitation';
+IF @buildingTechnologyId IS NOT NULL
 BEGIN
 	IF NOT EXISTS (SELECT * FROM dbo.Building WHERE Name = 'Old Earth Relic')
 		INSERT INTO dbo.Building(TechnologyId, Name, Cost) 
-		VALUES (@technologyId, 'Old Earth Relic', 40);
+		VALUES (@buildingTechnologyId, 'Old Earth Relic', 40);
 
 	IF NOT EXISTS (SELECT * FROM dbo.Building WHERE Name = 'Clinic')
 		INSERT INTO dbo.Building(TechnologyId, Name, Cost) 
-		VALUES (@technologyId, 'Clinic', 60);
+		VALUES (@buildingTechnologyId, 'Clinic', 60);
 END
 
-SELECT @technologyId = TechnologyId FROM Technology WHERE Name = 'Chemistry';
-IF @technologyId IS NOT NULL
+SELECT @buildingTechnologyId = TechnologyId FROM Technology WHERE Name = 'Chemistry';
+IF @buildingTechnologyId IS NOT NULL
 BEGIN
 	IF NOT EXISTS (SELECT * FROM dbo.Building WHERE Name = 'Laboratory')
 		INSERT INTO dbo.Building(TechnologyId, Name, Cost) 
-		VALUES (@technologyId, 'Laboratory', 80);
+		VALUES (@buildingTechnologyId, 'Laboratory', 80);
 
 	IF NOT EXISTS (SELECT * FROM dbo.Building WHERE Name = 'Recycler')
 		INSERT INTO dbo.Building(TechnologyId, Name, Cost) 
-		VALUES (@technologyId, 'Recycler', 75);
+		VALUES (@buildingTechnologyId, 'Recycler', 75);
 END
 
 
-SELECT @technologyId = TechnologyId FROM Technology WHERE Name = 'Ecology';
-IF @technologyId IS NOT NULL
+SELECT @buildingTechnologyId = TechnologyId FROM Technology WHERE Name = 'Ecology';
+IF @buildingTechnologyId IS NOT NULL
 BEGIN
 	IF NOT EXISTS (SELECT * FROM dbo.Building WHERE Name = 'Vivarium')
 		INSERT INTO dbo.Building(TechnologyId, Name, Cost) 
-		VALUES (@technologyId, 'Vivarium', 80);
+		VALUES (@buildingTechnologyId, 'Vivarium', 80);
 
 	IF NOT EXISTS (SELECT * FROM dbo.Building WHERE Name = 'Ultrasonic Fence')
 		INSERT INTO dbo.Building(TechnologyId, Name, Cost) 
-		VALUES (@technologyId, 'Ultrasonic Fence', 120);
+		VALUES (@buildingTechnologyId, 'Ultrasonic Fence', 120);
 END
--- src/BeyondEarthAppDb/Scripts/UnitData.sql
+++ src/BeyondEarthAppDb/Scripts/UnitData.sql
@@ -1,17 +1,17 @@
-﻿DECLARE @technologyId INT
+﻿DECLARE @unitTechnologyId INT
 
-SELECT @technologyId = TechnologyId FROM Technology WHERE Name = 'Habitation';
-IF @technologyId IS NOT NULL
+SELECT @unitTechnologyId = TechnologyId FROM Technology WHERE Name = 'Habitation';
+IF @unitTechnologyId IS NOT NULL
 BEGIN
 	IF NOT EXISTS (SELECT * FROM dbo.Unit WHERE Name = 'Worker')
 		INSERT INTO dbo.Unit(TechnologyId, Name, Cost, Strength) 
-		VALUES (@technologyId, 'Worker', 60, 0);
+		VALUES (@unitTechnologyId, 'Worker', 60, 0);
 
 	IF NOT EXISTS (SELECT * FROM dbo.Unit WHERE Name = 'Explorer')
 		INSERT INTO dbo.Unit(TechnologyId, Name, Cost, Strength) 
-		VALUES (@technologyId, 'Explorer', 40, 3);
+		VALUES (@unitTechnologyId, 'Explorer', 40, 3);
 
 	IF NOT EXISTS (SELECT * FROM dbo.Unit WHERE Name = 'Soldier')
 		INSERT INTO dbo.Unit(TechnologyId, Name, Cost, Strength) 
-		VALUES (@technologyId, 'Soldier', 50, 10);
+		VALUES (@unitTechnologyId, 'Soldier', 50, 10);
 END
