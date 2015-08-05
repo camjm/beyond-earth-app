ef86a11370e85e0ad73b3d0ccbb11d373290f28c
Cam <cam@mackay.net.nz>
2015-08-05 21:23:25 +1200

    added Affinity controller

---
+++ src/BeyondEarthApp.Web.Api/Controllers/V1/AffinitiesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Http;
+using BeyondEarthApp.Web.Api.Models;
+using BeyondEarthApp.Web.Common.Routing;
+
+namespace BeyondEarthApp.Web.Api.Controllers.V1
+{
+    [ApiVersion1RoutePrefix("games")]
+    public class AffinitiesController : ApiController
+    {
+        [Route("{affinityId:long}", Name = "GetAffinityRoute")]
+        public Affinity GetAffinity(long affinityId)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
