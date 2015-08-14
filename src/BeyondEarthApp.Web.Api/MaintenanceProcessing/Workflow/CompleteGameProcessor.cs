bcd5d81c8c429aa82df05f02f8c61211ee750632
Cam <cam@mackay.net.nz>
2015-08-14 21:44:44 +1200

    Game workflow processors

--- src/BeyondEarthApp.Web.Api/MaintenanceProcessing/Workflow/CompleteGameProcessor.cs
+++ src/BeyondEarthApp.Web.Api/MaintenanceProcessing/Workflow/CompleteGameProcessor.cs
@@ -1,16 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using BeyondEarthApp.Common;
+using BeyondEarthApp.Common.TypeMapping;
+using BeyondEarthApp.Data.Exceptions;
+using BeyondEarthApp.Data.QueryProcessors;
 using BeyondEarthApp.Web.Api.Models;
 
 namespace BeyondEarthApp.Web.Api.MaintenanceProcessing.Workflow
 {
     public class CompleteGameProcessor : ICompleteGameProcessor
     {
+        private readonly IDateTime _dateTime;
+        private readonly IAutoMapper _autoMapper;
+        private readonly IGameByIdQueryProcessor _gameByIdQueryProcessor;
+        private readonly IUpdateGameStatusQueryProcessor _updateGameStatusQueryProcessor;
+
+        public CompleteGameProcessor(
+            IDateTime dateTime, 
+            IAutoMapper autoMapper, 
+            IGameByIdQueryProcessor gameByIdQueryProcessor, 
+            IUpdateGameStatusQueryProcessor updateGameStatusQueryProcessor)
+        {
+            _dateTime = dateTime;
+            _autoMapper = autoMapper;
+            _gameByIdQueryProcessor = gameByIdQueryProcessor;
+            _updateGameStatusQueryProcessor = updateGameStatusQueryProcessor;
+        }
+
         public Game CompleteGame(long gameId)
         {
-            throw new NotImplementedException();
+            var gameEntity = _gameByIdQueryProcessor.GetGame(gameId);
+
+            if (gameEntity == null)
+            {
+                throw new RootObjectNotFoundException("Game not found");
+            }
+
+            // simulate some workflow logic...
+            if (gameEntity.Status.Name != "In Progress")
+            {
+                throw new BusinessRuleViolationException("Incorrect game status. Expected status of 'In Progress'.");
+            }
+
+            //gameEntity.CompletedDate = _dateTime.UtcNow;
+
+            _updateGameStatusQueryProcessor.UpdateGameStatus(gameEntity, "Completed");
+
+            var game = _autoMapper.Map<Game>(gameEntity);
+
+            return game;
         }
     }
 }
--- src/BeyondEarthApp.Web.Api/MaintenanceProcessing/Workflow/RestartGameProcessor.cs
+++ src/BeyondEarthApp.Web.Api/MaintenanceProcessing/Workflow/RestartGameProcessor.cs
@@ -1,16 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using BeyondEarthApp.Common;
+using BeyondEarthApp.Common.TypeMapping;
+using BeyondEarthApp.Data.Exceptions;
+using BeyondEarthApp.Data.QueryProcessors;
 using BeyondEarthApp.Web.Api.Models;
 
 namespace BeyondEarthApp.Web.Api.MaintenanceProcessing.Workflow
 {
     public class RestartGameProcessor : IRestartGameProcessor
     {
+        private readonly IAutoMapper _autoMapper;
+        private readonly IGameByIdQueryProcessor _gameByIdQueryProcessor;
+        private readonly IUpdateGameStatusQueryProcessor _updateGameStatusQueryProcessor;
+
+        public RestartGameProcessor(
+            IAutoMapper autoMapper, 
+            IGameByIdQueryProcessor gameByIdQueryProcessor, 
+            IUpdateGameStatusQueryProcessor updateGameStatusQueryProcessor)
+        {
+            _autoMapper = autoMapper;
+            _gameByIdQueryProcessor = gameByIdQueryProcessor;
+            _updateGameStatusQueryProcessor = updateGameStatusQueryProcessor;
+        }
+
         public Game RestartGame(long gameId)
         {
-            throw new NotImplementedException();
+            var gameEntity = _gameByIdQueryProcessor.GetGame(gameId);
+
+            if (gameEntity == null)
+            {
+                throw new RootObjectNotFoundException("Game not found");
+            }
+
+            // simulate some workflow logic...
+            if (gameEntity.Status.Name != "Completed")
+            {
+                throw new BusinessRuleViolationException("Incorrect game status. Expected status of 'Completed'.");
+            }
+
+            //gameEntity.CompletedDate = null;
+
+            _updateGameStatusQueryProcessor.UpdateGameStatus(gameEntity, "In Progress");
+
+            var game = _autoMapper.Map<Game>(gameEntity);
+
+            return game;
         }
     }
 }
