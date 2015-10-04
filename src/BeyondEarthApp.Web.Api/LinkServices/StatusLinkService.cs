64c9793bc72ca813d5a5466126035ea5f1303677
Cam <cam@mackay.net.nz>
2015-10-04 17:04:48 +1300

    Status Link Service

---
+++ src/BeyondEarthApp.Web.Api/LinkServices/StatusLinkService.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using BeyondEarthApp.Web.Api.Models;
+
+namespace BeyondEarthApp.Web.Api.LinkServices
+{
+    public class StatusLinkService : IStatusLinkService
+    {
+        private const string PathFragmentBase = "statuses";
+
+        private readonly ICommonLinkService _commonLinkService;
+
+        public StatusLinkService(ICommonLinkService commonLinkService)
+        {
+            _commonLinkService = commonLinkService;
+        }
+
+        public Link GetAllStatusesLink()
+        {
+            return _commonLinkService.GetLink(
+                PathFragmentBase,
+                "statuses",
+                HttpMethod.Get);
+        }
+    }
+}
