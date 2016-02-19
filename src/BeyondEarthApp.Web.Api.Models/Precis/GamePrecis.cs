ad6ba53fe8a30606c995c82ea0f06c42ee990557
Cam <cam@mackay.net.nz>
2016-02-20 12:02:40 +1300

    added precis types

---
+++ src/BeyondEarthApp.Web.Api.Models/Precis/GamePrecis.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeyondEarthApp.Web.Api.Models.Precis
+{
+    public class GamePrecis : ILinkContaining
+    {
+        public string Description { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public string Faction { get; set; }
+
+        public Status Status { get; set; }
+
+        public int TechnologyCount { get; set; }
+
+        #region Links
+
+        private List<Link> _links;
+
+        [Editable(false)]
+        public List<Link> Links
+        {
+            get { return _links ?? (_links = new List<Link>()); }
+            set { _links = value; }
+        }
+
+        public void AddLink(Link link)
+        {
+            Links.Add(link);
+        }
+
+        #endregion
+    }
+}
---
+++ src/BeyondEarthApp.Web.Api.Models/Precis/TechnologyPrecis.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeyondEarthApp.Web.Api.Models.Precis
+{
+    public class TechnologyPrecis : ILinkContaining
+    {
+        #region Links
+
+        private List<Link> _links;
+
+        [Editable(false)]
+        public List<Link> Links
+        {
+            get { return _links ?? (_links = new List<Link>()); }
+            set { _links = value; }
+        }
+
+        public void AddLink(Link link)
+        {
+            Links.Add(link);
+        }
+
+        #endregion
+    }
+}
