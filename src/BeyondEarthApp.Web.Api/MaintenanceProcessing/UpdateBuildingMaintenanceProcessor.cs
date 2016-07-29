ee02b0e9ebd0dc838afc61549c331fe4c3e150fc
Cam <cam@mackay.net.nz>
2016-07-30 10:50:11 +1200

    started infrastructure for Building and Unit API

---
+++ src/BeyondEarthApp.Web.Api/MaintenanceProcessing/UpdateBuildingMaintenanceProcessor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeyondEarthApp.Web.Api.MaintenanceProcessing
+{
+    public class UpdateBuildingMaintenanceProcessor
+    {
+    }
+}
