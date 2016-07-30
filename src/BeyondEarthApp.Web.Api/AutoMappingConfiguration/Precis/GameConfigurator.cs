710be3bf5ef179e21adc9e66243de47fc37d9ffe
Cam <cam@mackay.net.nz>
2016-07-30 15:45:26 +1200

    fix game precis mapping

--- src/BeyondEarthApp.Web.Api/AutoMappingConfiguration/Precis/GameConfigurator.cs
+++ src/BeyondEarthApp.Web.Api/AutoMappingConfiguration/Precis/GameConfigurator.cs
@@ -8,7 +8,12 @@ namespace BeyondEarthApp.Web.Api.AutoMappingConfiguration.Precis
         public void Configure(IAutoMapper mapper)
         {
             mapper.CreateMap<Game, Models.Precis.GamePrecis>()
-                .ForMember(x => x.Links, opts => opts.Ignore());
+                .ForMember(x => x.Links, opts => opts.Ignore())
+                //TODO: dummy data
+                .ForMember(x => x.CitiesCount, opts => opts.UseValue<int>(10))
+                .ForMember(x => x.Supremacy, opts => opts.UseValue<short>(12))
+                .ForMember(x => x.Harmony, opts => opts.UseValue<short>(3))
+                .ForMember(x => x.Purity, opts => opts.UseValue<short>(7));
         }
     }
 }
